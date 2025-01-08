b5a119ab74efbb181e0f69cf2df80e71ab815719
Bruno <brunolopezbarcia@icloud.com>
2025-01-08 20:25:28 +0100

    Add index route to return API status message

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from proxmox.proxmox_status import get_proxmox_status
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return 'API LEVANTADA'
+
 @app.route('/proxmox_status')
 def proxmox_status():
     return get_proxmox_status()
